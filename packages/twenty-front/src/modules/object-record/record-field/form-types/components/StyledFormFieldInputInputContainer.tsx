84962ef2253395213affa1a8a43b244a5f402fe6
Devessier <baptiste@devessier.fr>
2024-12-11 15:45:08 +0100

    refactor: prefer a local override

--- packages/twenty-front/src/modules/object-record/record-field/form-types/components/StyledFormFieldInputInputContainer.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/form-types/components/StyledFormFieldInputInputContainer.tsx
@@ -28,5 +28,4 @@ export const StyledFormFieldInputInputContainer = styled.div<{
   display: flex;
   overflow: ${({ multiline }) => (multiline ? 'auto' : 'hidden')};
   width: 100%;
-  position: relative;
 `;
